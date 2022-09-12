8b870420468ab50c84b06bd14130eedb06c3b469
Alan <wu.alan1434@gmail.com>
2022-09-12 13:54:34 +0100

    basic file system understanding done

--- TypeScripts/js/localFileSystem.js
+++ TypeScripts/js/localFileSystem.js
@@ -5,30 +5,46 @@ const indexedDB = window.indexedDB;
 const request = indexedDB.open("exampleDB", 1);
 // error handling
 request.onerror = function (event) {
-    console.error("can't open DB");
+    console.error("hello, can't open DB type error here");
     console.error(event);
 };
 request.onupgradeneeded = function () {
     const db = request.result;
     // createobjectstore is the same as tables
     // create keypath for primary key
-    const store = db.createObjectStore("ExampleDB", { keyPath: 'id' });
-    store.createIndex('Example_catagory-0', ['catagory-0'], { unique: false });
-    store.createIndex('Example_catagory-1', ['catagory-1', 'catagory-2'], { unique: false });
+    const store = db.createObjectStore("cars", { keyPath: 'id' });
+    store.createIndex('cars_colour', ['colour'], { unique: false });
+    store.createIndex('colour_and_make', ['colour', 'make'], { unique: false });
 };
 // note inserts and lookup don't usally belong in here
 request.onsuccess = function () {
     const db = request.result;
-    const transaction = db.transaction("ExampleDB", "readwrite");
-    const store = transaction.objectStore("ExampleDB");
-    const index = store.index("Example_catagory-0");
-    const makeIndex = store.index("Example_catagory-1");
-    store.put({ 'id': 1, 'catagory-0': 2, 'catagory-2': 3 });
+    const transaction = db.transaction("cars", "readwrite");
+    const store = transaction.objectStore("cars");
+    console.log('REQUEST ON SUCESS: ', store);
+    const index = store.index('cars_colour');
+    const makeIndex = store.index("colour_and_make");
+    store.put({ id: 1, colour: 'red', make: 'tesla' });
     const idQuery = store.get(1);
     const indexQuery = index.getAll(2);
     const makeIndexQuery = makeIndex.get('');
     idQuery.onsuccess = function () {
         console.log("idQuery sucess! ", idQuery.result);
+    };
+    idQuery.onerror = function (event) {
+        console.log("idQuery error! ", event);
+    };
+    indexQuery.onsuccess = function () {
+        console.log("indexQuery sucess! ", indexQuery.result);
+    };
+    indexQuery.onerror = function (event) {
+        console.log("indexQuery error! ", event);
+    };
+    makeIndexQuery.onsuccess = function () {
+        console.log("makeIndexQuery sucess! ", makeIndexQuery.result);
+    };
+    makeIndexQuery.onerror = function (event) {
+        console.log("makeIndexQuery error! ", event);
     };
     // close db
     transaction.oncomplete = function () {
