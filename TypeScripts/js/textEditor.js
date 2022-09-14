e569cf9718bdc79df1fe5192c28e76232272d8f8
Alan <wu.alan1434@gmail.com>
2022-09-14 19:59:00 +0100

    some refactoring, coffeeScript to typeScript

---
+++ TypeScripts/js/textEditor.js
@@ -0,0 +1,27 @@
+"use strict";
+//functions here
+Object.defineProperty(exports, "__esModule", { value: true });
+let handleMouseMove = (event) => {
+    event = event || window.event;
+    console.log(event);
+};
+let setKeyDown = (event) => {
+    var input = document.getElementById('editor');
+    return false;
+};
+let setKeyUp = (event) => {
+    return false;
+};
+let stopShortCuts = (event) => {
+    // using keycodes to filter out keys
+    var special = [9];
+    if (event.keycode in special) {
+        event.preventDefault();
+    }
+    ;
+    return true;
+};
+var container = document.getElementById('Container');
+container.addEventListener('keydown', setKeyDown);
+container.addEventListener('keyup', setKeyUp);
+container.onkeydown = stopShortCuts;
